bfa6f946092963b77124f5fa751c5d10d37560aa
九级浪 <413612464@qq.com>
2023-12-12 17:23:26 +0800

    inif: watch两个参数

---
+++ src/App/06-watch两个参数.vue
@@ -0,0 +1,57 @@
+<!-- 
+  06-watch两个参数
+-->
+
+<script setup>
+import { ref, watch } from "vue";
+
+/**
+ * immediate: count
+ */
+const count = ref(0);
+const addCount = () => count.value++;
+watch(count, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+});
+
+/**
+ * deep: obj
+ */
+const obj = ref({ nama: 'hxl', age: 18 });
+const addAge = () => obj.value.age++;
+watch(obj, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+});
+
+/**
+ * deep: arr
+ */
+const arr = ref([1, 2]);
+const addArr = () => arr.value[0]++;
+watch(arr, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+});
+
+</script>
+
+<template>
+  <div>
+    <!-- immediate -->
+    <div>
+      {{ count }}
+      <button @click="addCount">count</button>
+    </div>
+
+    <!-- deep: obj -->
+    <div>
+      {{ obj }}
+      <button @click="addAge">obj</button>
+    </div>
+
+    <!-- deep: arr -->
+    <div>
+      {{ arr }}
+      <button @click="addArr">arr</button>
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
-import App from './App/05-watch.vue';
+import App from './App/06-watch两个参数.vue';
 
 createApp(App).mount('#app');
