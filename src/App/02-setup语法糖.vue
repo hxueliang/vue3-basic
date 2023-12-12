703692d1935e8b152e542b5d1c6878765b8ba4f4
九级浪 <413612464@qq.com>
2023-12-12 15:23:45 +0800

    feat: setup语法糖

--- src/App/02-setup语法糖.vue
+++ src/App/02-setup语法糖.vue
@@ -1,20 +1,11 @@
 <!-- 
   02-setup语法糖
 -->
-<script>
-export default {
-  setup() {
-    // 数据
-    const message = 'hello vue3';
-    // 函数
-    const getMessage = () => console.log(message);
-
-    return {
-      message,
-      getMessage,
-    };
-  },
-};
+<script setup>
+// 数据
+const message = 'hello vue3';
+// 函数
+const getMessage = () => console.log(message);
 </script>
 
 <template>
