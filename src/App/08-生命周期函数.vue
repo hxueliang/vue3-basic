8116d2d532022cdc6a21535cc5851474a15e697f
九级浪 <413612464@qq.com>
2023-12-12 21:18:08 +0800

    init: 生命周期函数

---
+++ src/App/08-生命周期函数.vue
@@ -0,0 +1,11 @@
+<!-- 
+  08-生命周期函数
+-->
+
+<script setup>
+</script>
+
+<template>
+  <div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
-import App from './App/07-watch侦听对象指定属性.vue';
+import App from './App/08-生命周期函数.vue';
 
 createApp(App).mount('#app');
