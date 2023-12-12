29c50381b38c6d8c9834218b52111a7a7c59a78f
九级浪 <413612464@qq.com>
2023-12-12 21:58:09 +0800

    feat: 父传子

--- src/App/09-父传子.vue
+++ src/App/09-父传子.vue
@@ -3,6 +3,7 @@
 -->
 
 <script setup>
+import SonCom from '@/components/09/SonCom.vue';
 import { ref } from 'vue';
 
 const count = ref(0);
@@ -14,5 +15,9 @@ const count = ref(0);
       父组件
       <button @click="() => count++">+count</button>
     </h3>
+
+    <!-- 传普通值 -->
+    <!-- 传响应式值 -->
+    <SonCom msg="父传给子的message" :count="count" />
   </div>
 </template>
--- src/components/09/SonCom.vue
+++ src/components/09/SonCom.vue
@@ -3,11 +3,33 @@
 -->
 
 <script setup>
+/**
+ * 注意：由于写了 setup，所以无法直接配置 props 选项
+ * 所以：此处需要借助于“编译器宏” 函数，接收父组件传递的数据
+ * 
+ * defineProps 原理：就是编译阶段的一个标识，实际编译器解析时，遇到后会进行编译转换
+ */
+const props = defineProps({
+  msg: String,
+  count: Number,
+});
+
+console.log(props);
+// 使用 msg，需要 props.msg
+console.log(props.msg);
+console.log(props.count);
 </script>
 
 <template>
   <div class="son">
     子组件
+    <div>
+      <!-- 使用 msg，直接 msg，不需要 props.msg -->
+      {{ msg }}
+    </div>
+    <div>
+      {{ count }}
+    </div>
   </div>
 </template>
 
