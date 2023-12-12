3a7a41bb023fa598b9377ff5522654eccc1f1b3e
九级浪 <413612464@qq.com>
2023-12-12 20:29:06 +0800

    feat: watch侦听对象指定属性

--- src/App/07-watch侦听对象指定属性.vue
+++ src/App/07-watch侦听对象指定属性.vue
@@ -3,10 +3,14 @@
 -->
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const obj = ref({ nama: 'hxl', age: 18 });
 const addAge = () => obj.value.age++;
+watch(
+  () => obj.value.age,
+  (newValue, oldValue) => console.log(newValue, oldValue)
+)
 
 </script>
 
