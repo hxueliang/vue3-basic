74654d705efd11e73d1b466682ea298c97cec5fe
九级浪 <413612464@qq.com>
2023-12-12 16:50:30 +0800

    feat: watch

--- src/App/05-watch.vue
+++ src/App/05-watch.vue
@@ -1,15 +1,32 @@
 <!-- 
   05-watch
+
+  作用：
+    侦听一个或者多个数据变化，数据变化时执行回调函数
+
+  两个额外的参数：
+    1、immediate(立即执行) 
+    2、deep(深度侦听)
 -->
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const count = ref(0);
 const name = ref('Vue');
 
 const addCount = () => count.value++;
 const setName = () => name.value += 12345;
+
+// 侦听一个值
+watch(count, (newValue, oldValue) => {
+  console.log('1个值', newValue, oldValue);
+});
+
+// 侦听多个值
+watch([count, name], (newArr, oldArr) => {
+  console.log('多个值', newArr, oldArr);
+});
 
 </script>
 
