4d924696382977dc3414f0fd8295306c495ca7f5
九级浪 <413612464@qq.com>
2023-12-12 22:32:51 +0800

    feat: 子传父

--- src/App/10-子传父.vue
+++ src/App/10-子传父.vue
@@ -3,16 +3,27 @@
 -->
 
 <script setup>
+import { ref } from 'vue';
 import SonCom from '@/components/10/SonCom.vue';
 
+const name = ref('');
+const getName = _name => {
+  name.value = _name;
+};
+
+const age = ref(0);
+const getAge = _age => {
+  age.value = _age;
+};
 </script>
 
 <template>
   <div>
     <h3>
       父组件
+      - {{ name }} - {{ age }}
     </h3>
 
-    <SonCom />
+    <SonCom @getName="getName" @getAge="getAge" />
   </div>
 </template>
--- src/components/10/SonCom.vue
+++ src/components/10/SonCom.vue
@@ -3,11 +3,32 @@
 -->
 
 <script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['getName', 'getAge']);
+
+/**
+ * 传普通字符串
+ */
+const sendName = () => {
+  emit('getName', 'hxl');
+};
+
+/**
+ * 传应式数据
+ */
+const age = ref(0);
+const sendAge = () => {
+  age.value++;
+  emit('getAge', age);
+};
 </script>
 
 <template>
   <div class="son">
     子组件
+    - <button @click="sendName">set Name</button>
+    - <button @click="sendAge">set Age</button>
   </div>
 </template>
 
