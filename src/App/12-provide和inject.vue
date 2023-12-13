74d111860daccbf330ad75e048dc57f52aa116af
九级浪 <413612464@qq.com>
2023-12-13 16:25:38 +0800

    feat: provide和inject

--- src/App/12-provide和inject.vue
+++ src/App/12-provide和inject.vue
@@ -1,9 +1,23 @@
 <!-- 
   12-provide和inject
+
+  作用：项层组件向任意的底层组件传递数据和方法，实现跨层组件通信
 -->
 
 <script setup>
+import { provide, ref } from 'vue';
 import CenterCom from '@/components/12/CenterCom.vue';
+
+// 普通数据
+provide('fontColor', 'blue');
+
+// 响应式数据
+const bg = ref('yellow');
+provide('bgColor', bg);
+setTimeout(() => bg.value = 'green', 1000);
+
+// 提供方法
+provide('changeColor', color => bg.value = color);
 </script>
 
 <template>
--- src/components/12/BottomCom.vue
+++ src/components/12/BottomCom.vue
@@ -3,12 +3,31 @@
 -->
 
 <script setup>
+import { inject } from 'vue';
+
+// 接收字体颜色
+const fontColor = inject('fontColor');
+
+// 接收背景颜色
+const bgColor = inject('bgColor');
+
+// 接收修改颜色方法
+const changeColor = inject('changeColor');
+
+// 随机修改颜色
+const colorArr = ['yellow', 'green', 'pink', 'wheat', 'yellowgreen'];
+const handleRandom = () => {
+  const index = Math.floor(Math.random() * colorArr.length);
+  changeColor(colorArr[index]);
+};
 </script>
 
 <template>
   <div>
-    <h4>
+    <h4 :style="{ color: fontColor, background: bgColor }">
       底层组件
+      <button @click="() => changeColor('grey')">grey</button>
+      <button @click="handleRandom">random color</button>
     </h4>
   </div>
 </template>
