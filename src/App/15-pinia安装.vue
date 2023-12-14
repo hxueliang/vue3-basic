743eec9364ede5fd4462cd84913ce90292a69ae4
九级浪 <413612464@qq.com>
2023-12-14 13:19:41 +0800

    feat: pinia安装

--- src/App/15-pinia安装.vue
+++ src/App/15-pinia安装.vue
@@ -1,5 +1,11 @@
 <!-- 
   15-pinia安装
+
+  是什么：是 vue 的最新 状态管理工具，是 vuex 的 替代品
+  1、提供更加简单的API（去掉了 mutation）
+  2、提供符合，组合式风格的API（和 vue了 新语法统一）
+  3、去掉了 modules 的概念，每一个 store 都是一个独立的模块
+  4、配合 Typescript 更加友好，提供可靠的类型推断
 -->
 
 <script setup>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 import App from './App/15-pinia安装.vue';
 
 const app = createApp(App);
+const painia = createPinia();
 
+app.use(painia);
 app.mount('#app');
