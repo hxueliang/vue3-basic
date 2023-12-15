e15b2180f56cfd40be690a9d4036a909a2db669a
九级浪 <413612464@qq.com>
2023-12-15 16:03:14 +0800

    chore: 增加eslint规则

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,5 +7,16 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+    // 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
+    'no-undef': 'error'
   }
-}
+};
