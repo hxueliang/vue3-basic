7480709b6055434c1ae424cde24b43b0be38daac
九级浪 <413612464@qq.com>
2023-12-16 14:19:31 +0800

    init: 大件事案例

---
+++ src/App/20-大件事案例.vue
@@ -0,0 +1,12 @@
+<!-- 
+  20-大件事案例
+-->
+
+<script setup>
+</script>
+
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import persist from 'pinia-plugin-persistedstate';
 
-import App from './App/App.vue';
+import App from './App/20-大件事案例.vue';
 import router from './router';
+
+// 20-大件事案例
+import '@/assets/style/20/main.scss';
 
 const painia = createPinia();
 painia.use(persist);
