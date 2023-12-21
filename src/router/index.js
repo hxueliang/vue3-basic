7d2f5226189c12ecb178478e4828bbc0812be73c
九级浪 <413612464@qq.com>
2023-12-21 14:26:28 +0800

    feat: 大件事案例-登录访问拦截

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import { useUserStore } from '@/store';
 import router20 from './20';
+
+const BASE_PATH = '20';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     ...router20,
   ],
+});
+
+router.beforeEach((to) => {
+  const useStore = useUserStore();
+  const is20 = to.path.split('/')[1] === BASE_PATH;
+
+  if (is20 && !useStore.token && to.path !== `/${BASE_PATH}/login`) {
+    return `${BASE_PATH}/login`;
+  }
 });
 
 export default router;
